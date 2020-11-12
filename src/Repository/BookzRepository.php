7f3977c26fdcaf30006f860b6d70c7b9803dc63e
Alxche.msi <alxche@mail.ru>
2020-11-12 21:02:02 +0300

    удалены тесты. добавлена миграция. почишен репозит

--- src/Repository/BookzRepository.php
+++ src/Repository/BookzRepository.php
@@ -19,32 +19,5 @@ class BookzRepository extends ServiceEntityRepository
         parent::__construct($registry, Bookz::class);
     }
 
-    // /**
-    //  * @return Bookz[] Returns an array of Bookz objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('b.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Bookz
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+   
 }
